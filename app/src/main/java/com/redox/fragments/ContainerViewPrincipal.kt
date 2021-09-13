95b12e6beda5069959421a26d9525a260a572e1a
Christian Axel Serrano Sandoval ( Chiles ) <maxgaa001@gmail.com>
2021-09-13 16:36:02 -0500

    primera vista
    carrusel de la primera vista

---
+++ app/src/main/java/com/redox/fragments/ContainerViewPrincipal.kt
@@ -0,0 +1,16 @@
+package com.redox.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ContainerViewPrincipal : Fragment(R.layout.fragment_container_view_principal) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+}
