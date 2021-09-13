a2edf972226f0e19cd5f1072684a9385f7abeaf5
Christian Axel Serrano Sandoval ( Chiles ) <maxgaa001@gmail.com>
2021-09-13 17:17:34 -0500

    imagenes y clases para las imagenes

--- app/src/main/java/com/redox/fragments/MainActivity.kt
+++ app/src/main/java/com/redox/fragments/MainActivity.kt
@@ -2,10 +2,32 @@ package com.redox.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(
+                R.id.fragmentContainer,
+                ContainerViewPrincipal()
+            )
+            .commit()
+
     }
+
+    fun changeFragments(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(
+                R.id.fragmentContainer,
+                fragment
+            )
+            .addToBackStack(fragment.tag)
+            .commit()
+    }
+
 }
---
+++ app/src/main/java/com/redox/fragments/Picture.kt
@@ -0,0 +1,25 @@
+package com.redox.fragments
+
+import android.os.Parcelable
+import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
+
+@JsonClass(generateAdapter = true)
+@Parcelize
+class Picture(
+    var descripcion: String? = "Descripcion",
+    var source: Int,
+    var save: Boolean = false,
+    var like: Boolean = false,
+
+) : Parcelable
+
+@JsonClass(generateAdapter = true)
+class listPicture(
+    val arrayPictures: Array<Picture> = arrayOf(
+        Picture("Don Quijote", R.drawable.ic_don_quijote),
+        Picture("Harry Potter", R.drawable.ic_harry_potter),
+        Picture("Dracula", R.drawable.ic_dracula),
+        Picture("El jugador", R.drawable.ic_jugador)
+    )
+)
