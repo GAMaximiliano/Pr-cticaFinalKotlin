ef69d55b1ee44a33f9a0b3a3eac2cef789e4ffab
Christian Axel Serrano Sandoval ( Chiles ) <maxgaa001@gmail.com>
2021-09-13 16:53:40 -0500

    vista completa de la imagen

---
+++ app/src/main/java/com/redox/fragments/Complete.kt
@@ -0,0 +1,14 @@
+package com.redox.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Complete : Fragment(R.layout.fragment_complete) {
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
