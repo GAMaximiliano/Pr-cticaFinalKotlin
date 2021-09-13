dd5ba209239d6275eef49385e9ae02c23b03ade3
Christian Axel Serrano Sandoval ( Chiles ) <maxgaa001@gmail.com>
2021-09-13 16:51:15 -0500

    vista de detalles
    detalles diseno

---
+++ app/src/main/java/com/redox/fragments/ViewDetail.kt
@@ -0,0 +1,16 @@
+package com.redox.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ViewDetail : Fragment(R.layout.fragment_view_detail) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+}
